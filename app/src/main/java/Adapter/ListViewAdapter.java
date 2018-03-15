b3145d0f692bfacf346e0f59e917bdc3e0dff568
kevnokeeffe <kevokeeffe@gmail.com>
2018-03-15 10:08:42 +0000

    delete and views fixed

--- app/src/main/java/Adapter/ListViewAdapter.java
+++ app/src/main/java/Adapter/ListViewAdapter.java
@@ -11,6 +11,7 @@ import android.widget.Toast;
 
 import java.util.List;
 
+import Data.DataBaseHandler;
 import Model.ListItem;
 import Model.Order;
 import ie.mywaiter.mywaiter.NotesActivity;
@@ -19,7 +20,9 @@ import ie.mywaiter.mywaiter.R;
 import ie.mywaiter.mywaiter.SplashScreen;
 import ie.mywaiter.mywaiter.TablesActivity;
 import ie.mywaiter.mywaiter.ViewOrders;
-
+import android.content.DialogInterface;
+import android.os.Build;
+import android.support.v7.app.AlertDialog;
 /**
  * Created by Kev on 25/02/2018.
  */
@@ -44,13 +47,38 @@ public class ListViewAdapter extends RecyclerView.Adapter<ListViewAdapter.ViewHo
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        Order order = orders.get(position);
-
+        final Order order = orders.get(position);
         holder.starter.setText(order.getStarter());
         holder.main.setText(order.getMain());
         holder.desert.setText(order.getDesert());
         holder.drink.setText(order.getDrink());
-
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder;
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    builder = new AlertDialog.Builder(context, android.R.style.Theme_Material_Dialog_Alert);
+                } else {
+                    builder = new AlertDialog.Builder(context);
+                }
+                builder.setTitle("Delete order")
+                        .setMessage("Are you sure you want to delete this order?")
+                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                DataBaseHandler db = new DataBaseHandler(context);
+                                db.deleteOrder(order);
+                                orders.remove(order);
+                                notifyDataSetChanged();
+                            }
+                        })
+                        .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                            }
+                        })
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .show();
+            }
+        });
     }
 
 
--- app/src/main/java/ie/mywaiter/mywaiter/NewOrder.java
+++ app/src/main/java/ie/mywaiter/mywaiter/NewOrder.java
@@ -87,16 +87,12 @@ public class NewOrder extends AppCompatActivity {
     //when the order button is pressed the string values from the widgets populate the database
     public void addOrderButtonPressed (View view)
     {
-//        starter = startWidg.getText().toString();
-//        main = mainWidg.getText().toString();
-//        desert = desertWidg.getText().toString();
-//        drink = drinkWidg.getText().toString();
+        String starter = startWidg.getText().toString();
+        String main = mainWidg.getText().toString();
+        String desert = desertWidg.getText().toString();
+        String drink = drinkWidg.getText().toString();
         db.addOrder(new Order(starter,main,desert,drink));
         Intent intent = new Intent(NewOrder.this, ViewOrders.class);
-//        intent.putExtra("starter", starter);
-//        intent.putExtra("main", main);
-//        intent.putExtra("desserts", desert);
-//        intent.putExtra("drinks", drink);
         startActivity(intent);
     }
 
